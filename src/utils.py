138f94c951ebccddbbc30410887db57818587bba
TylerBikaun <tyler.bikaun@woodplc.com>
2020-11-06 12:39:06 +0800

    added sequential data building and updated aux functions

--- src/utils.py
+++ src/utils.py
@@ -29,6 +29,9 @@ class DataPreparation:
         self.min_occurence = self.utils_config['min_occurence']
         self.date = date.today().strftime('%d-%m-%Y')
         self.max_seq_len = self.utils_config['max_sequence_length']
+        self.pad_token = '<PAD>'
+        self.sos_token = '<SOS>'
+        self.eos_token = '<EOS>'
 
         if self.task_type=='NER':
             print('NER LIFE :)!')
@@ -72,23 +75,24 @@ class DataPreparation:
             self._prepare_sequences(split=split, data=data)
 
             # Trim and Pad sequences
-            if split == 'train':
-                self._trim_sequences(split=split)
-                # self._pad_sequences(data=data['kv_pairs'])
-                pass
+            self._trim_sequences(split=split)
+            self._pad_sequences(split=split)
+            self._add_special_tokens(split=split)
 
-            # if split == 'train':
-            #     print('Building vocabularies and mappings')
-            #     self._build_vocabs()
-            #     self._word2idx()
-            #     self._idx2word()
-            #     self._tag2idx()
-            #     self._idx2tag()
+            if split == 'train':
+                print('Building vocabularies and mappings from training data')
+                self._build_vocabs()
+                self._word2idx()
+                self._idx2word()
+                self._tag2idx()
+                self._idx2tag()
+
+            self.convert_sequences(split=split)
         
         # Save results (add datetime and counts)
-        self._save_json(self.utils_config['data_root_path']+f'\CoNLL2003_{self.date}.json', self.dataset)
+        self._save_json(path=self.utils_config['data_root_path']+f'\CoNLL2003_{self.date}.json', data=self.dataset)
 
-    def _prepare_sequences(self, split, data):
+    def _prepare_sequences(self, split : str, data):
         """ Converts corpus into sequence-tag tuples.
         TODO:
             - Currently works for CoNLL2003 NER copora
@@ -97,7 +101,7 @@ class DataPreparation:
         corpus = data['corpus']
         docs = [list(group) for k, group in groupby(corpus, lambda x: len(x) == 1) if not k]
 
-        self.dataset[split]['kv_pairs'] = list()
+        self.dataset[split]['seq_tags_pairs'] = list()
         data = dict()
         # Split docs into sequences and tags
         doc_count = 0
@@ -106,17 +110,17 @@ class DataPreparation:
             tags = [token.split()[-1] for token in doc]
             data[doc_count] = (sequence, tags)
             doc_count += 1
-        self.dataset[split]['kv_pairs'] = data
+        self.dataset[split]['seq_tags_pairs'] = data
 
     def _build_vocabs(self, split='train'):
-        """ Built off of training set - out of vocab tokens will be marked as <UNK>
-        
-        Two vocabularies - word and tag
+        """ Builds vocabularies off of words and tags. These are built from training data so out of vocabulary tokens
+        will be marked as <UNK> when being converted into numerical vectors. """
 
-        Minimum occurence (frequency) cut-off is implemented to keep models reasonable sized (CURRENTLY NOT IMPLEMENTED... will need to do enumeration if used)
-        """
         # Get list of words in corpus
-        word_list = list(itertools.chain.from_iterable([doc.split() for doc in [" ".join(seq) for seq, tag in self.dataset[split]['kv_pairs']]]))
+        word_list = list(itertools.chain.from_iterable([doc.split() for doc in [" ".join(seq) for seq, tag in self.dataset[split]['seq_tags_pairs'].values()]]))
+
+        # Remove special_tokens (these are added explicitly later)
+        word_list = [word for word in word_list if word not in list(self.special_tokens.keys())]
 
         word_freqs = {}
         # Get word frequencies
@@ -139,12 +143,25 @@ class DataPreparation:
         # Build word and tag vocabularies
         # comprehensions are a bit nasty... but effective!
         self.vocab_words = word_list_keep
-        self.vocab_tags = list(set(list(itertools.chain.from_iterable([doc.split() for doc in [" ".join(tag) for seq, tag in self.dataset[split]['kv_pairs']]]))))
+        tag_list = list(itertools.chain.from_iterable([doc.split() for doc in [" ".join(tag) for seq, tag in self.dataset[split]['seq_tags_pairs'].values()]]))
+        # Remove special_tokens (these are added explicitly later)
+        tag_list = [tag for tag in tag_list if tag not in list(self.special_tokens.keys())]
+        self.vocab_tags = list(set(tag_list))
+        
+        
+        # Add special_tokens to vocabs
+        self.vocab_words = list(self.special_tokens.keys()) + self.vocab_words
+        self.vocab_tags = list(self.special_tokens.keys()) + self.vocab_tags
         print(f'Size of vocabularies - Word: {len(self.vocab_words)} Tag: {len(self.vocab_tags)}')
+
+
+        # Save vocabularies to disk
+        vocabs = {'words': self.vocab_words, 'tags': self.vocab_tags}
+        self._save_json(path=self.utils_config['data_root_path']+f'\CoNLL2003_vocabs_{self.date}.json',data=vocabs)
         
     def _word2idx(self):
         """ Built off of training set - out of vocab tokens are <UNK>"""
-        self.word2idx = {word:idx+len(self.special_tokens) for idx, word in enumerate(self.vocab_words)}
+        self.word2idx = {word:idx for idx, word in enumerate(self.vocab_words)}
         # add special tokens to mapping
         self.word2idx = {**self.special_tokens, **self.word2idx, }
 
@@ -161,39 +178,55 @@ class DataPreparation:
         self.idx2tag = {idx:tag for tag, idx in self.tag2idx.items()}
 
     def _trim_sequences(self, split: str):
-        """ Trims sequences to the maximum allowable length 
-        
-        Arguments
-        ---------
-            data : list, tuples
-                List of key-value pairs (sequence, tags)
-        """
-        for idx, pair in self.dataset[split]['kv_pairs'].items():            
+        """ Trims sequences to the maximum allowable length """
+        for idx, pair in self.dataset[split]['seq_tags_pairs'].items():            
             seq, tags = pair
-            self.dataset[split]['kv_pairs'][idx] = (seq[:self.max_seq_len], tags[:self.max_seq_len])
+            self.dataset[split]['seq_tags_pairs'][idx] = (seq[:self.max_seq_len], tags[:self.max_seq_len])
 
-    def _pad_sequences(self, data):
+    def _pad_sequences(self, split: str):
         """ Pads sequences up to the maximum allowable length """
-        pass
+        for idx, pair in self.dataset[split]['seq_tags_pairs'].items():
+            seq, tags = pair
+            if len(seq) < self.max_seq_len:
+                # probably a better way to do this, but comprehension is easy. TODO: fix dodgy code!
+                seq = seq + [self.pad_token for _ in range(self.max_seq_len - len(seq))]
+                tags = tags + [self.pad_token for _ in range(self.max_seq_len - len(tags))]
+                self.dataset[split]['seq_tags_pairs'][idx] = (seq, tags)
 
+    def _add_special_tokens(self, split: str):
+        """ Adds special tokens such as <EOS>, <SOS> onto sequences """
+        for idx, pair in self.dataset[split]['seq_tags_pairs'].items():
+            seq, tags = pair
+            seq = [self.sos_token] + seq + [self.eos_token]
+            tags = [self.sos_token] + tags + [self.eos_token]
+            self.dataset[split]['seq_tags_pairs'][idx] = (seq, tags)
 
-    def convert_sequences(self):
-        """ Converts sequences of tokens and tags to their indexed forms for each split in the dataset
+    def convert_sequences(self, split: str):
+        """
+        Converts sequences of tokens and tags to their indexed forms for each split in the dataset
         
-        Note: any word in the sequence that isn't in the vocab will be replaced with <UNK> TODO: investigate how this might impact the SVAE word_dropout methodology """
+        Note: any word in the sequence that isn't in the vocab will be replaced with <UNK>
+        TODO: investigate how this might impact the SVAE word_dropout methodology """
 
-        # sequences
+        # If word or tag is not in the sequence, change with <UNK>
+        # unsure if the output tags need to be changed? I assume not as the output tags are known. TODO: verify logic.
 
-        # tags
-        pass
+        self.dataset[split]['seq_tags_pairs_enc'] = dict()  # enc -> integer encoded pairs
+        for idx, pair in self.dataset[split]['seq_tags_pairs'].items():            
+            seq, tags = pair
+            # Sequences
+            seq_enc = [self.word2idx.get(word, self.word2idx['<UNK>']) for word in seq]
+            # Tags
+            tags_enc = [self.tag2idx.get(tag, self.word2idx['<UNK>']) for tag in tags]
+
+            self.dataset[split]['seq_tags_pairs_enc'][idx] = (seq_enc, tags_enc)
 
     def normalise(self):
         pass
 
-    def _save_json(self, path, data):
+    def _save_json(self, path: str, data: dict):
         with open(path, 'w') as outfile:
             json.dump(data, outfile, indent=4)
-
 
 # Misc functions below
 def trim_padded_seqs(batch_lengths: Tensor, batch_sequences: Tensor, pad_idx: int) -> Tensor:
