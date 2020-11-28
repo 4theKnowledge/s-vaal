80bb9d1568844033838c8685c69bf416000d08d3
4theKnowledge <tylerbikaun@gmail.com>
2020-11-29 07:47:10 +0800

    Updated mongodb to be more flexible, post on demand rather than bulk posting after each experiment

--- src/connections.py
+++ src/connections.py
@@ -6,7 +6,7 @@ import yaml
 import sys, traceback
 from pymongo import MongoClient
 from datetime import datetime
-
+import uuid
 
 def load_config(path=None):
     """ Loads configuration file from disk
@@ -47,14 +47,55 @@ class Mongo:
     def post(self, data: dict):
         """ Posts to a mongodb collection """
         try:
-            post_data = {"post timestamp": datetime.now()}
+            post_data = {"created": datetime.now()}
             post_data.update(data)
             self.collection.insert_one(post_data)
-            print('Succesfully posted data to mongo db collection')
+            print(f'{datetime.now()}: Succesfully posted data to mongo db collection')
         except Exception as e:
             print(f'Error posting data to collection - \n{e}\n')
             traceback.print_exc(file=sys.stdout)
-
+            
+    def post_exp_init(self, exp_name=None, settings=None, runs=None):
+        """ Posts initial data for start of experiment """
+        try:
+            # Make placeholder for run data
+            run_ph = {str(run): None for run in range(1,runs+1,1)}
+            
+            id = str(uuid.uuid4())
+            post_data = {"_id": id,
+                         "created": datetime.now(),
+                         "name": exp_name,
+                         "info": {"start timestamp": None,
+                                  "finish timestamp": None,
+                                  "run time": None},
+                         "settings": settings,
+                         "results": run_ph,
+                         "samples": run_ph,
+                         "predictions": {
+                             "results": run_ph,
+                             "statistics": run_ph}
+                         }
+            self.collection.insert_one(post_data)
+            print(f'{datetime.now()}: Succesfully posted data to mongo db collection')
+            return id
+        
+        except Exception as e:
+            print(f'Error posting data to collection - \n{e}\n')
+            traceback.print_exc(file=sys.stdout)
+    
+    def post_exp_data(self, id, run, field=None, sub_field=None, data=None):
+        """ Adds data to an experiment via post """
+        try:
+            self.collection.update_one({"_id": id},
+                                       {"$set": {f'{field}.{run}' if sub_field is None else f'{field}.{sub_field}.{run}': data}},
+                                       False,
+                                       True
+                                       )
+            print(f'{datetime.now()}: Succesfully updated data in mongo db collection')
+        except Exception as e:
+            print(f'Error posting data to collection - \n{e}\n')
+            traceback.print_exc(file=sys.stdout)
+    
     # WORK IN PROGRESS...
     def find(self, field_name, operation=None, value=None):
         """ Searches mongodb collection based on field name and an operation and/or value
@@ -68,11 +109,26 @@ class Mongo:
             value : float
                 Value to apply operation to or search mongo db collection for
         """
-        # mongo_find(collection, field_name='name', value='john snow')
-        result = self.collection.find({field_name: value}).count()
+        result = self.collection.find_one({field_name: value})
         print(result)
 
 
 if __name__ == '__main__':
-    # Mongo(collection_name='experiments')
-    pass
+    mongo = Mongo(collection_name='experiments')
+    
+    id = mongo.post_exp_init(runs=5)
+    
+    field = 'results'
+    run = '1'
+    data = 'hello world'
+    mongo.post_exp_data(id=id, run=run, field=field, data=data)
+    
+    run = '2'
+    data = 'goodbye world'
+    mongo.post_exp_data(id=id, run=run, field=field, data=data)
+    
+    run = '1'
+    field = 'predictions'
+    sub_field = 'results'
+    data = 'tyler was here'
+    mongo.post_exp_data(id=id, run=run, field=field, sub_field=sub_field, data=data)
